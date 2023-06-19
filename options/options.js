9d6bf76dc2512e9e3bcb1981e3cca47741f1b28a
Crowdbotics <team@crowdbotics.com>
2023-06-19 03:45:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sportsvision-41586",
+  url: "https://sportsvision-41586.botics.co",
+  api: "https://sportsvision-41586.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
